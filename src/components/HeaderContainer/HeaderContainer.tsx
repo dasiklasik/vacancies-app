f66f2444c5427317a2bae18517d31ebd598cf6da
dasiklasik <daryasamsonowicz@gmail.com>
2023-05-19 18:56:56 +0300

    fix: fix styles for header

--- src/components/HeaderContainer/HeaderContainer.tsx
+++ src/components/HeaderContainer/HeaderContainer.tsx
@@ -1,25 +1,27 @@
-import {Header} from "@mantine/core";
+import {Flex, Header, Title} from "@mantine/core";
 import React from "react";
-import {NavLink, Route, Router} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import styles from './HeaderContainer.module.css';
+import {LogoIcon} from "../Icons/LogoIcon";
 
 
 export const HeaderContainer = () => {
     return (
         <Header height={'84px'}>
-            <div className={styles.wrapper}>
-                <div>
-                    Logo
-                </div>
+            <Flex align="center" gap={280} className={styles.wrapper}>
+                <Flex align="center" gap={12} className={styles.logo}>
+                    <LogoIcon/>
+                    <Title className={styles.logo__title} order={1}>Jobored</Title>
+                </Flex>
                 <div className={styles.navigation}>
                     <nav>
                         <ul>
-                            <li><NavLink className={({isActive}) => isActive ? 'active' : ''} to='/vacancies'>Поиск вакансий</NavLink></li>
-                            <li><NavLink className={({isActive}) => isActive ? 'active' : ''} to='/favorites'>Избранное</NavLink></li>
+                            <li><NavLink className={({isActive}) => isActive ? styles.active : ''} to='/vacancies'>Поиск вакансий</NavLink></li>
+                            <li><NavLink className={({isActive}) => isActive ? styles.active : ''} to='/favorites'>Избранное</NavLink></li>
                         </ul>
                     </nav>
                 </div>
-            </div>
+            </Flex>
         </Header>
     )
 }
